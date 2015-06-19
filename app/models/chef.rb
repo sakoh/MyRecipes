e0457a1086f37afcd1df54d5872186ab0ac408f3
Sako Hartounian <sakohartounian@yahoo.com>
2015-06-19 20:17:39 +0000

    validation and validation tests added for chefs

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,3 +1,8 @@
 class Chef < ActiveRecord::Base
-  
+  before_save { self.email = email.downcase }
+  validates :chefname, presence: true, length: {minimum: 3, maximum: 40}
+  VALID_EMAIL_REGEX = /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+  validates :email, presence: true, length: {maximum: 105}, 
+                                    uniqueness: {case_sensitive: false},
+                                    format: {with: VALID_EMAIL_REGEX }
 end
