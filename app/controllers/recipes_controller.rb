0838ea4675c837a0d778a94448f1897e41438d8b
Sako Hartounian <sakohartounian@yahoo.com>
2015-07-15 00:52:05 +0000

    error validation added

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,35 @@
+class RecipesController < ApplicationController
+  before_action :set_recipe, only: [:show, :edit]
+  
+  def index
+    @recipes = Recipe.all
+  end
+  
+  def show
+  end
+  
+  def new
+    @recipe = Recipe.new
+  end
+  
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.chef = Chef.find(2)
+    
+    if @recipe.save
+      flash[:success] = "New Recipe created"
+      redirect_to recipes_path
+    else
+      render :new
+    end
+  end
+  
+  private
+    def set_recipe
+      @recipe = Recipe.find(params[:id])
+    end
+  
+    def recipe_params
+      params.require(:recipe).permit(:name, :summary, :description)
+    end
+end
