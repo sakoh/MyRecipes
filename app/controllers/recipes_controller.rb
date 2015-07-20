d1214b16c623cdbb23404f577b5605f1e7d89e94
sakoh <sakohartounian@yahoo.com>
2015-07-20 16:13:01 -0700

    pagination added

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,7 +2,7 @@ class RecipesController < ApplicationController
   before_action :set_recipe, only: [:show, :edit, :update, :like]
   
   def index
-    @recipes = Recipe.all.sort_by{|likes| likes.thumbs_up_total}.reverse
+    @recipes = Recipe.paginate(page: params[:page], per_page: 4)
   end
   
   def show
